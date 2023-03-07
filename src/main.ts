6e6ba230d8dee82b4343d11780b9acd3f2d67f3c
Gaurav Rana <smartgauravrana@gmail.com>
2023-03-07 21:48:44 +0530

    Dummy controllers setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3001);
 }
 bootstrap();
